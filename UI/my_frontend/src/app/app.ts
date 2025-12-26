d50fafe6ea7c8b66dc35e08ff0bd6d0a1e3dab7b
Kirankumarkarri <KarriKiran.Kumar@cognizant.com>
2025-12-26 12:26:16 +0530

    Register form 26 Dec

--- UI/my_frontend/src/app/app.ts
+++ UI/my_frontend/src/app/app.ts
@@ -1,9 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+
+import {ToastModule} from 'primeng/toast';
+
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PasswordModule,InputTextModule,ButtonModule,ReactiveFormsModule,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- UI/my_frontend/src/app/registration/registration.ts
+++ UI/my_frontend/src/app/registration/registration.ts
@@ -1,11 +1,99 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { PasswordModule } from 'primeng/password';
+// import { SelectModule } from 'primeng/select';
+// import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { InputTextModule } from 'primeng/inputtext';
+import {MessageModule} from 'primeng/message';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import {CardModule} from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
+
+
+
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [PasswordModule, 
+    ReactiveFormsModule, 
+    CommonModule, 
+    ToastModule,
+    InputTextModule,
+    MessageModule,
+  FloatLabelModule,
+  CardModule,
+ButtonModule,
+RouterModule],
   templateUrl: './registration.html',
   styleUrl: './registration.css',
+  providers: [MessageService]
 })
 export class Registration {
+
+  // risks = [
+  //   { label: 'Low', value: 'LOW' },
+  //   { label: 'Medium', value: 'MEDIUM' },
+  //   { label: 'High', value: 'HIGH' }
+  // ];
+
+  registerForm: any;
+
+  constructor(private fb: FormBuilder, private messageService: MessageService) {
+    this.registerForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      age: ['', [Validators.required, Validators.min(18)]],
+      username: ['', Validators.required],
+      // risk: ['', Validators.required],
+      // amount: ['', Validators.required],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(/^(?=.*[A-Z])(?=.*\d).+$/)
+      ]],
+      confirmPassword: ['', Validators.required]
+    }, { validators: this.passwordMatch });
+  }
+
+  passwordMatch(control: AbstractControl) {
+    const pwd = control.get('password')?.value;
+    const cpwd = control.get('confirmPassword')?.value;
+    return pwd === cpwd ? null : { mismatch: true };
+  }
+
+  isInvalid(controlName: string): boolean {
+    const control = this.registerForm.get(controlName);
+    // Returns true if the control is invalid AND has been touched/dirty
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+
+  isDarkMode = false;
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    const element = document.querySelector('html');
+    if (element) {
+      element.classList.toggle('my-app-dark');
+    }
+  }
+
+  submit() {
+    if (this.registerForm.valid) {
+
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Registration Successful',
+        detail: 'User registered successfully'
+      });
+      this.registerForm.reset();
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
 
 }
